5b142b8c3bacc0ee59bb884e538b6e180fa30b2f
Roger Ivan Gurdian <87393759+rg-neivor@users.noreply.github.com>
2022-12-14 21:48:54 -0600

    First commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-S">Lista Quehaceres App</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
