9cbc2c80c042449ef03c2d39acb7700aed6b71d1
Roger Ivan Gurdian <ivan.gurdian@outlook.com>
2022-12-14 22:52:36 -0600

    Reading tasks

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="container">
     <h2 class="text-center mt-S">Lista Quehaceres App</h2>
+
+    <div class="d-flex">
+      <input type="text" placeholder="Ingrese Tarea" class="form-control">
+      <button class="btn btn-warning rounded-0">Ingresar</button>
+    </div>
+
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Tarea</th>
+          <th scope="col">Estado</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>{{task.name}}</td>
+          <td>{{task.status}}</td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,7 +41,18 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  data() {
+  return {
+    tasks: [
+      {
+        name: 'Aprender Vue',
+        status: 'Hecho'
+      }
+    ]
   }
+}
 }
 </script>
 
