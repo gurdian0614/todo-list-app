67f411601893393938b6922d46dba3131077e23c
Roger Ivan Gurdian <ivan.gurdian@outlook.com>
2022-12-16 09:25:19 -0600

    Fixing deploy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    disableHostCheck: true,
     allowedHosts: [
       'https://todo-list-app-gurdian.up.railway.app/'
     ]
