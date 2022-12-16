e3263c18915e191fea821aed885742244d980a37
Roger Ivan Gurdian <ivan.gurdian@outlook.com>
2022-12-16 08:37:52 -0600

    Adding vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'https://todo-list-app-gurdian.up.railway.app/'
+    ]
+  }
 })
